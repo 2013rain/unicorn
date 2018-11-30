84867e743d16bf1065d1cbd6b08558876f5fb7cc
2013rain <936466975@qq.com>
2018-11-30 14:21:53 +0800

    first commit

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://unicorn.wangfuyu.com.cn/',
+    'authkey' => '8RrxpgYsggc8dsuKfKzigG8wSnWgixUF',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
