93b46417d28cabae6f60d47621acb78af8fcadfc
2013rain <936466975@qq.com>
2018-12-09 20:39:23 +0800

    wfy modify

--- project/modules/admin/classes/admin.class.php
+++ project/modules/admin/classes/admin.class.php
@@ -112,11 +112,11 @@ class admin {
 			}
 			if($_value['parentid'] == 0 || $_value['m']=='') continue;
 			if($classname) {
-				// $string .= "<a href='javascript:;' $classname><em>".L($_value['name'])."</em></a><span>|</span>";
-				$string .= "<a href='javascript:;' $classname><em>".$_value['tabname']."</em></a><span>|</span>";
+				 $string .= "<a href='javascript:;' $classname><em>".L($_value['name'])."</em></a><span>|</span>";
+
 			} else {
-				// $string .= "<a href='?m=".$_value['m']."&c=".$_value['c']."&a=".$_value['a']."&menuid=$parentid&pc_hash=$pc_hash".'&'.$_value['data']."' $classname><em>".L($_value['name'])."</em></a><span>|</span>";
-				$string .= "<a href='?m=".$_value['m']."&c=".$_value['c']."&a=".$_value['a']."&menuid=$parentid&pc_hash=$pc_hash".'&'.$_value['data']."' $classname><em>".$_value['tabname']."</em></a><span>|</span>";
+				 $string .= "<a href='?m=".$_value['m']."&c=".$_value['c']."&a=".$_value['a']."&menuid=$parentid&pc_hash=$pc_hash".'&'.$_value['data']."' $classname><em>".L($_value['name'])."</em></a><span>|</span>";
+				
 			}
 		}
 		$string = substr($string,0,-14);
